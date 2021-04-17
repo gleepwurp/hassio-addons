d4008bf07a956b4f67d594a59437251684a61d83
Yannick Moussette <yannick.moussette@gmail.com>
2021-04-17 14:53:26 -0400

    Dropped Home-assistant version metadata

--- s3-backup/rootfs/usr/bin/amazon-s3-backup/amazon-s3-backup.py
+++ s3-backup/rootfs/usr/bin/amazon-s3-backup/amazon-s3-backup.py
@@ -145,7 +145,8 @@ def upload_file(file: Path, s3_bucket: S3Bucket, supervisor_api: SupervisorAPI):
         snapshot_detail = supervisor_api.get_snapshot(slug)
         # DEBUG
         logger.debug(f"SNAPSHOT DETAIL: {snapshot_detail}")
-        metadata_keys = ["type", "name", "date", "homeassistant"]
+        # metadata_keys = ["type", "name", "date", "homeassistant"]
+        metadata_keys = ["type", "name", "date"]
         metadata = {k: snapshot_detail[k]
                     for k in snapshot_detail if k in metadata_keys}
     except SupervisorAPIError as err:
