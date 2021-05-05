4f9d720089d9bb733ef2d6e7078890a1c24be33c
Yannick Moussette <yannick.moussette@gmail.com>
2021-05-05 17:38:09 -0400

    Added Upload Indicator in log

--- s3-backup/rootfs/usr/bin/amazon-s3-backup/amazon-s3-backup.py
+++ s3-backup/rootfs/usr/bin/amazon-s3-backup/amazon-s3-backup.py
@@ -153,8 +153,9 @@ def upload_file(file: Path, s3_bucket: S3Bucket, supervisor_api: SupervisorAPI):
         logger.warning(
             f"Error getting snapshot info from Home Assistant Supervisor API : {err}")
 
+    logger.info(f"Uploading {str(file)} to S3...")
     s3_bucket.upload_file(str(file), metadata)
-
+    logger.info(f"Done.")
 
 if __name__ == "__main__":
     set_log_level(os.environ.get("LOG_LEVEL"))
